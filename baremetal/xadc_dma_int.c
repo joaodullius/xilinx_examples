c837141fdaa8c5c6a408db5366b2dd8979deda05
João Dullius <joaodullius@bpmrep.com.br>
2019-01-23 11:50:22 -0200

    Update xadc_dma_int.c

--- baremetal/xadc_dma_int.c
+++ baremetal/xadc_dma_int.c
@@ -1,4 +1,4 @@
-/*Copyright (c) 2018, Adam Taylor
+/*Copyright (c) 2018, João Dullius (based on Adam Taylor)
 All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
